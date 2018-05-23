2ebd64aa7cf2cc10979481164f568c4028f2412a
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-05-23 17:30:37 +0100

    Add a Result component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Button from './Button.js';
+import Result from './Result.js';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Button />
+        <Result />
       </div>
     );
   }
