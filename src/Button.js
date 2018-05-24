22535efe2a20a44ff5d01637e93c3fcf3674eaf8
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-05-24 09:15:09 +0100

    Create one function to share across all Button instances

--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
 class Button extends Component {
+    handleClick = () => {
+        this.props.onClickFunction(this.props.incrementValue)
+    };
+
     render() {
         return (
-            <button onClick={() => this.props.onClickFunction(this.props.incrementValue)}>
+            <button onClick={this.handleClick}>
                 +{this.props.incrementValue}
             </button>
         );
