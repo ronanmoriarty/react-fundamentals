6d610d7704170cbb019c647b1c600cab1dc7c94b
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-05-24 09:57:42 +0100

    Add Card component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import React, { Component } from 'react';
-import Button from './Button.js';
-import Result from './Result.js';
+import Card from './Card.js';
 
 class App extends Component {
-  state = {
-    counter: 10
-  };
-
-  incrementCounter = (incrementValue) => {
-    this.setState((previousState) => ({
-      counter: previousState.counter + incrementValue
-    }));
-  };
-
-  render() {
-    return (
-      <div>
-        <Button onClickFunction={this.incrementCounter} incrementValue={1} />
-        <Button onClickFunction={this.incrementCounter} incrementValue={5} />
-        <Button onClickFunction={this.incrementCounter} incrementValue={10} />
-        <Button onClickFunction={this.incrementCounter} incrementValue={100} />
-        <Result counter={this.state.counter} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Card />
+        );
+    }
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = (props) => {
+    return (
+        <div>
+            <img src="http://placehold.it/75" />
+            <div>
+                <div>Name here...</div>
+                <div>Company name here...</div>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
